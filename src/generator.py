d3429569cfbd9fa9569c2a9241ce4e4f20ed456c
Tomas <315267532@qq.com>
2025-09-02 13:38:03 +0800

    Update generator.py

--- src/generator.py
+++ src/generator.py
@@ -68,7 +68,7 @@ def generate_report(nodes):
         
         report += f"| {node['protocol']} | {node['host']} | {node['port']} | {latency:.2f} | {quality} |\n"
     
-    with open('REPORT.md', 'w') as f:
+    with open('https://github.com/Eleven1985/sub-scription/edit/main/README.md', 'w') as f:
         f.write(report)
     
     logging.info("Generated node quality report")
